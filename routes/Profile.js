5e823780ac8f3f280a1936a9cae9081146a118ae
florent ayot <florent.ayot33@gmail.com>
2019-11-27 16:28:23 +0800

    still working on profile routes

--- routes/Profile.js
+++ routes/Profile.js
@@ -28,16 +28,16 @@ const fetchJson = require("fetch-json");
 // });
 
 
-router.get("/:merbershipId", async(req, res) => {
+router.get("/:merbershipId", async (req, res) => {
   try {
     const headers = {
       "X-API-KEY": process.env.TRACKER_API_KEY
     };
-    const membershipId  = req.params;
-    console.log(membershipId)
-    console.log(typeof membershipId)
+    const membershipId = req.params;
+    //console.log(membershipId)
+    // console.log(typeof membershipId)
     const asdf = membershipId.merbershipId
-    console.log(asdf)
+    //console.log(asdf)
     const destinyId = await fetch(
       `${process.env.TRACKER_API_URL2}/User/GetMembershipsById/${asdf}/-1/`,
       {
@@ -45,13 +45,26 @@ router.get("/:merbershipId", async(req, res) => {
       }
     );
     const data = await destinyId.json();
-    console.log(data);
+    //console.log(data.Response)
+    const membershipType = data.Response.destinyMemberships[1].membershipType
+    const idDestiny = data.Response.destinyMemberships[1].membershipId
+    console.log(membershipType);
+    console.log(idDestiny);
+    const persoInfos = await fetch(
+      `${process.env.TRACKER_API_URL2}/Destiny2/${membershipType}/Profile/${idDestiny}/?components=100`,
+      {
+        headers
+      }
+    );
+    const data2 = await persoInfos.json();
+    console.log(data2.Response.profile.data.characterIds)
   } catch (error) {
-        console.error(err);
-        res.status(500).json({
-          message: "Server Error"
-        });
-      }});
+    console.error(err);
+    res.status(500).json({  
+      message: "Server Error"
+    });
+  }
+});
 
 
 module.exports = router;
